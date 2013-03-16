6a1153b2c8828eea75aa5f92c2de669ae4a79492
Kaushik <kaosman@gmail.com>
2013-03-16 17:16:03 -0400

    Module 2 complete

--- src/dao/DataAccessObject.java
+++ src/dao/DataAccessObject.java
@@ -1,24 +1,24 @@
 package dao;
 
-import model.Hotel;
-import model.Guest;
-import model.Room;
-import model.Booking;
-
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Types;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
+import model.Guest;
+
 public class DataAccessObject 
 {
 	private Connection connectionHotel = null;
 	private ServiceProvider serviceProvider = null;
+	private Date sdate;
+	private Date edate;
 
 	public DataAccessObject() 
 	{
@@ -84,31 +84,8 @@ public class DataAccessObject
 		return false;
 	}
 
-	public boolean deleteExistingGuest(Guest existingGuest) 
-	{
-		// The agent can also delete the information for an existing guest, if required.
-		final String sql = "DELETE FROM Guest " +
-				"WHERE guestid = ? ";
-
-		try
-		{
-
-			PreparedStatement statement = connectionHotel.prepareStatement(sql);
-			statement.setInt(1, existingGuest.getGuestID());
-			statement.execute();
-
-			return true;
-
-		}catch(SQLException e)
-		{
-			e.printStackTrace();
-		}
-		return false;
-		
-	}
-	
 	//Module 2 - Booking query
-	public Hotel getAvailableHotels(String startdate, String enddate, String hotelname, 
+	public ArrayList<String> getAvailableHotels(String startdate, String enddate, String hotelname, 
 			String city, String roomprice, String roomtype)
 	{
 		// Once the guest information has been entered, the agent can then query
@@ -118,19 +95,67 @@ public class DataAccessObject
 		// the corresponding condition is not applied (e.g., if city is left
 		// blank, all cities are considered).
 		
-		Hotel hotel = null;
-		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-		try {
-			Date sdate = (Date) dateFormat.parse(startdate);
-			Date edate = (Date) dateFormat.parse(enddate);
-		} catch (ParseException e) {
-			System.err.println("Unable to parse date to SQL format");
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
+		ArrayList<String> availableHotelNames = new ArrayList<String>();		
+
+		String sql = "SELECT H.hotelid,H.hotelName, H.city, R.roomno,R.price,R.roomtype " +
+				"FROM HOTEL H "+
+				"JOIN Room R ON H.hotelid = R.hotelid "+
+				"JOIN Booking B ON H.hotelid = B.hotelid "+
+				"WHERE (( CAST(? AS DATE) NOT BETWEEN B.startdate AND B.enddate) AND " +
+				"(CAST(? AS DATE) NOT BETWEEN B.startdate AND B.enddate) " +
+				"AND H.hotelName = ? "+
+				"AND H.city = ? AND R.price = ROUND(CAST(? AS NUMERIC),2) AND R.roomtype = ?)";
+		try{
+			PreparedStatement statement = connectionHotel.prepareStatement(sql);
+
+			if(startdate.isEmpty())
+				statement.setNull(1, Types.NULL);
+			else
+				statement.setString(1, startdate);
+			
+			if(enddate.isEmpty())	
+				statement.setNull(2, Types.NULL);
+			else
+				statement.setString(2, enddate);
+
+			if(hotelname.isEmpty())
+				statement.setNull(3, Types.NULL);
+			else
+				statement.setString(3, hotelname);
+			
+			if(city.isEmpty())
+				statement.setNull(4, Types.NULL);
+			else
+				statement.setString(4, city);
+			
+			if(roomprice.isEmpty())
+				statement.setNull(5, Types.NULL);
+			else
+				statement.setString(5, roomprice);
+
+			if(roomtype.isEmpty())
+				statement.setNull(6, Types.NULL);
+			else
+				statement.setString(6, roomtype);
+
+			ResultSet resultSet = statement.executeQuery();
+			while(resultSet.next()){
+				String result = "H.hotelid = " + resultSet.getString(1) +
+						" H.hotelName = " + resultSet.getString(2) +
+						" H.city = " + resultSet.getString(3) +
+						" R.roomno = " + resultSet.getString(4) +
+						" R.price = " + resultSet.getString(5) +
+						" R.roomtype = " + resultSet.getString(6);
+				availableHotelNames.add(result);
+			}
+			
+			
+		}catch(SQLException e)
+		{
 			e.printStackTrace();
 		}
-		
-		final String sql = "SELECT * FROM HOTEL WHERE hotelName ";
-		
-		return hotel;
+		return availableHotelNames;
 	}
 
 	public void getRoomInfo()
@@ -165,7 +190,5 @@ public class DataAccessObject
 		// Each bill printed should also be logged into the billing log for the
 		// accounting purposes.
 	}
-
-
 
 }
